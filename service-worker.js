c6ae682761de7bcc34b80b5198c7a264ab4bbfe8
hari prasad <hariprasad@haris-MacBook-Pro.local>
2026-05-28 13:51:29 +0530

    Fixed PWA duplicate code

--- service-worker.js
+++ service-worker.js
@@ -2,22 +2,20 @@ const CACHE_NAME = "showy-hrms-v1";
 
 const urlsToCache = [
 
-"/",
-"/index.html",
-"/dashboard.html",
-"/css/style.css",
-"/assets/logo.png"
+"/Showy-HRMS-ERP-System/",
+"/Showy-HRMS-ERP-System/index.html",
+"/Showy-HRMS-ERP-System/dashboard.html",
+"/Showy-HRMS-ERP-System/css/style.css",
+"/Showy-HRMS-ERP-System/assets/logo.png"
 
 ];
 
-self.addEventListener(
-"install",
-event=>{
+self.addEventListener("install", event => {
 
 event.waitUntil(
 
 caches.open(CACHE_NAME)
-.then(cache=>{
+.then(cache => {
 
 return cache.addAll(urlsToCache);
 
@@ -27,14 +25,12 @@ return cache.addAll(urlsToCache);
 
 });
 
-self.addEventListener(
-"fetch",
-event=>{
+self.addEventListener("fetch", event => {
 
 event.respondWith(
 
 caches.match(event.request)
-.then(response=>{
+.then(response => {
 
 return response || fetch(event.request);
 
